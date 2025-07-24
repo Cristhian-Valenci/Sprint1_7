13af2647b80c5865a8b9909724c6300c776b8a96
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-07-24 11:18:47 +0200

    Correccion del ejercicio 2 del nivel 1

--- tests/EstudianteTest.php
+++ tests/EstudianteTest.php
@@ -6,15 +6,52 @@ use PHPUnit\Framework\TestCase;
 
 class EstudianteTest extends TestCase {
 
-    public function test_CalcularGradoDelEstudiante() {
-
+    public function test_ComprobarPrimeraDivisionBien() {
         $Estudiante = new Estudiante();
 
-        $this->assertGreaterThanOrEqual("Primera Division", $Estudiante->calcularGradoDelEstudiante(60));
-        $this->assertGreaterThanOrEqual("Segunda Division", $Estudiante->calcularGradoDelEstudiante(45));
-        $this->assertGreaterThanOrEqual("Tercera Division", $Estudiante->calcularGradoDelEstudiante(33));
-        $this->assertLessThanOrEqual("Estudiante reprobado", $Estudiante->calcularGradoDelEstudiante(32));
-        
+        $this->assertEquals("Primera Division", $Estudiante->calcularGradoDelEstudiante(60));
+    }
+    
+    public function test_ComprobarPrimeraDivisionMal() {
+        $Estudiante = new Estudiante();
+
+        $this->assertNotEquals("Primera Division", $Estudiante->calcularGradoDelEstudiante(59));
+    }
+   
+    public function test_ComprobarSegundaDivisionBien() {
+        $Estudiante = new Estudiante();
+
+        $this->assertEquals("Segunda Division", $Estudiante->calcularGradoDelEstudiante(46));
+    }
+   
+    public function test_ComprobarSegundaDivisionMal() {
+        $Estudiante = new Estudiante();
+
+        $this->assertNotEquals("Segunda Division", $Estudiante->calcularGradoDelEstudiante(44));
+    }
+   
+    public function test_ComprobarTerceraDivisionBien() {
+        $Estudiante = new Estudiante();
+
+        $this->assertEquals("Tercera Division", $Estudiante->calcularGradoDelEstudiante(33));
+    }
+   
+    public function test_ComprobarTerceraDivisionMal() {
+        $Estudiante = new Estudiante();
+
+        $this->assertNotEquals("Tercera Division", $Estudiante->calcularGradoDelEstudiante(32));
+    }
+   
+    public function test_ComprobarEstudianteReprobadoBien() {
+        $Estudiante = new Estudiante();
+
+        $this->assertEquals("Estudiante reprobado", $Estudiante->calcularGradoDelEstudiante(32));
+    }
+   
+    public function test_ComprobarEstudianteReprobadoMal() {
+        $Estudiante = new Estudiante();
+
+        $this->assertNotEquals("Estudiante reprobado", $Estudiante->calcularGradoDelEstudiante(35));
     }
 }
 ?>
